3435ed1c05b0683bb4fc2e941c75019dc1ff96d8
gmahima <mahimagangavarapu@gmail.com>
2020-09-28 20:28:13 +0530

    add xstate and checkbox machine

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
 import Head from 'next/head'
 import styled from 'styled-components'
 import tw from 'twin.macro'
+import {Machine} from 'xstate'
+import {useMachine} from '@xstate/react'
 
+const CheckBoxMachine = Machine({
+  states: {
+    switched_off: {
+      on: {
+        TOGGLE_SWITCH: {
+          target: "switched_on"
+        }
+      }
+    },
+    switched_on: {
+      on: {
+        TOGGLE_SWITCH: {
+          target: "switched_off"
+        }
+      }
+    }
+  },
+  initial: "switched_on"
+})
 const Container = styled.div`
 ${tw `
- bg-gray-900 min-h-screen
+
 `}
 `
 
 export default function Home() {
+  const [state, send] = useMachine(CheckBoxMachine)
+  console.log(state.value)
   return (
     <Container>
-      asdf
+      <p>
+        checkbox
+      </p>
+      <button onClick= {() => (send("TOGGLE_SWITCH"))}>toggle switch</button>
+      <input type="checkbox" checked={state.value === "switched_on"} onChange= {() => (send("TOGGLE_SWITCH"))}/>
     </Container>
   )
 }
